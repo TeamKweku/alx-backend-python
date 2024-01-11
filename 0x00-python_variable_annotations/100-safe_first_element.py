e94fec7bdbee117b6e9ee8c38a6bfb37dbaef5d2
teamkweku <teamkweku@outlook.com>
2024-01-11 15:42:16 +0000

    annotating a function that takes a sequence and returns optional arguments of type any or None

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""this module arguments a function with the correct
+    duck-typed annotations
+"""
+from typing import Sequence, Optional, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Safely returns the first element of a sequence or None
+    if the sequence is empty.
+
+    Parameters:
+        lst (typing.Sequence): A sequence of elements.
+
+    Returns:
+        Optional[Any]: The first element of the sequence or None
+        if the sequence is empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
