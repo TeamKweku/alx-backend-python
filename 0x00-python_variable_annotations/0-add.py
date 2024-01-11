0f1e6922bb68e8e2116eeae5b90023857b2454bd
teamkweku <teamkweku@outlook.com>
2024-01-11 11:49:17 +0000

    a typed-annotated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""a module that contains an add function"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Parameters:
+        a (float): The first floating-point number.
+        b (float): The second floating-point number.
+
+    Returns:
+        float: The sum of the two input numbers.
+    """
+    return a + b
+
