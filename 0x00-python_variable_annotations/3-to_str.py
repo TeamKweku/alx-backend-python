295149609f9249e9023effa773f9c4be5ab5e666
teamkweku <teamkweku@outlook.com>
2024-01-11 13:47:08 +0000

    type annotated to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""module that implements a to_string function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Parameters:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
