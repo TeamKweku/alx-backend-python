7ecf1d8e6d6e7a056a0163ac8ceb543ac84058ac
teamkweku <teamkweku@outlook.com>
2024-01-11 14:15:21 +0000

    type-annotated funx that takes a mixed list and returns float

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,8 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     Returns the sum of a list of integers and floats.
 
     Parameters:
-        mxd_lst (List[Union[int, float]]): The input list of integers and floats.
+        mxd_lst (List[Union[int, float]]): The input list of
+        integers and floats.
 
     Returns:
         float: The sum of the input list.
