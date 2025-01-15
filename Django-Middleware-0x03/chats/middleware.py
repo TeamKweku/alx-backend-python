333e42a254b870bce308c6c4c5bb32e7d59f0df7
teamkweku <teamkweku@outlook.com>
2025-01-15 17:09:20 +0000

    fix: checker errors for RolePermissions

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -78,7 +78,7 @@ class OffensiveLanguageMiddleware:
         return request.META.get("REMOTE_ADDR")
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # Define admin-only URL patterns
