9ca785880f8cf9b8d2bf88df6814516fbaf23a3f
teamkweku <teamkweku@outlook.com>
2025-01-15 16:59:36 +0000

    feat: adding middleware to restrict access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 
 class RequestLoggingMiddleware:
@@ -16,4 +17,20 @@ class RequestLoggingMiddleware:
         # Call the next middleware/view
         response = self.get_response(request)
 
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_time = datetime.now().time()
+        # Check if time is between 9 AM and 6 PM
+        if current_time.hour < 9 or current_time.hour >= 18:
+            return HttpResponseForbidden(
+                "Access to chat is only allowed between 9 AM and 6 PM"
+            )
+
+        response = self.get_response(request)
         return response
